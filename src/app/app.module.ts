c4c1ec5d303feba401e628c936c6cb8e9a15cb04
awardnasc <awardnasc@gmail.com>
2018-02-13 15:25:00 -0500

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { LoginPageComponent } from './login-page/login-page.component';
-import {RouterModule, Routes} from "@angular/router";
-import {AF} from "../providers/af";
+import {RouterModule, Routes} from '@angular/router';
+import {AF} from '../providers/af';
 import { HomePageComponent } from './home-page/home-page.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyA3QeqVOHH8OGTAipaGWfjEYt2QRXce9JE",
-  authDomain: "rotcfit-33ec0.firebaseapp.com",
-  databaseURL: "https://rotcfit-33ec0.firebaseio.com",
-  storageBucket: "rotcfit-33ec0.appspot.com",
-  messagingSenderId: "21604736537"
+  apiKey: 'AIzaSyA3QeqVOHH8OGTAipaGWfjEYt2QRXce9JE',
+  authDomain: 'rotcfit-33ec0.firebaseapp.com',
+  databaseURL: 'https://rotcfit-33ec0.firebaseio.com',
+  storageBucket: 'rotcfit-33ec0.appspot.com',
+  messagingSenderId: '21604736537'
 };
 
 const googleMapsCore = AgmCoreModule.forRoot({
